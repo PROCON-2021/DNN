41d18742efb8dfc270ed3aa96678e6da7415c838
ioComk <rw110407@gmail.com>
2021-09-01 19:44:50 +0900

    一括のDataloaderでラベルを種目に応じて作成

--- datasetloader.py
+++ datasetloader.py
@@ -1,10 +1,11 @@
-import numpy as np
 import os
+import numpy as np
 from pathlib import Path
 from torch.utils.data import Dataset
 
-class BaseDataset(Dataset):
-    def __init__(self, path_name):
+class TrainValDataset(Dataset):
+
+    def __init__(self, path_name, type_):
 
         self.path = Path(path_name)
         self.csv_path_list = []
@@ -12,25 +13,33 @@ class BaseDataset(Dataset):
         for dir_name in self.path.glob('*/*.csv'):
             self.csv_path_list.append(str(dir_name))
 
+        # 種目に応じてラベルを設定
+        if type_ == 'thighs':
+            self.label = (
+                'thighs_cant_keep',
+                'thighs_cant_raise',
+                'thighs_cant_slow',
+                'thighs_cant_stop',
+                'thighs_cant_strech',
+                'thighs_correct',
+            )
+        elif type_ == 'abs':
+            self.label = (
+                'abs_correct',
+                'abs_keep_shoulders',
+                'abs_raise',
+                'abs_recoil',
+            )
+        elif type_ == 'shoulders':
+            self.label = (
+                'shoulders_correct',
+                'shoulders_pull',
+                'shoulders_sides',
+                'shoulders_trunk',
+            )
+
     def __len__(self):
         return len(self.csv_path_list)
-
-    def __getitem__(self, idx):
-        raise NotImplementedError
-
-class ThighsTrainValDataset(BaseDataset):
-
-    def __init__(self, path_name):
-        super().__init__(path_name)
-
-        self.label = (
-            'thighs_cant_keep',
-            'thighs_cant_raise',
-            'thighs_cant_slow',
-            'thighs_cant_stop',
-            'thighs_cant_strech',
-            'thighs_correct',
-        )
 
     def __getitem__(self, idx):
 
