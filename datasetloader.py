d6f8883d90bf0e8347fe4ca6e81839eaaaa3ff75
ioComk <rw110407@gmail.com>
2021-09-01 17:53:47 +0900

    信号をシフトせずに一括で入力できるように修正

--- datasetloader.py
+++ datasetloader.py
@@ -1,45 +1,45 @@
 import numpy as np
+import os
 from pathlib import Path
 from torch.utils.data import Dataset
-from tqdm import tqdm
 
-class MyDataset(Dataset):
-    def __init__(self, path, frame_range=20, key='train'):
+class BaseDataset(Dataset):
+    def __init__(self, path_name):
 
-        # ! TODO
-        # ! テスト用の処理を実装
-        # ! シリアルで送られてくる信号をバッファして云々
+        self.path = Path(path_name)
+        self.csv_path_list = []
 
-        dir = Path(path)
-        file_list = list(dir.iterdir())
-
-        # 入力データの範囲
-        self.frame_range = frame_range
-
-        # 配列確保用
-        tmp = np.loadtxt(file_list[0], delimiter=',', dtype=np.float32)
-
-        # データとラベルを格納する配列
-        # data : [siglen x ch x n_files]
-        # label: [siglen x label x n_files]
-        self.data = np.zeros([tmp.shape[0], tmp.shape[1], len(file_list)]).astype(np.float32)
-        self.label = np.zeros([tmp.shape[0], 1, len(file_list)]).astype(np.float32)
-
-        # tqdm設定
-        proc = tqdm(total=len(file_list), desc='Import dataset and label')
-
-        for i, file in enumerate(file_list):
-            self.data[:,:,i] = np.loadtxt(file, delimiter=',', dtype='float32')
-
-            # patience: 1
-            # trainee : 0
-            self.label[:, :,i] = 1 if (file.stem.split("_")[0] == 'patience') else 0
-
-            proc.update()
+        for dir_name in self.path.glob('*/*.csv'):
+            self.csv_path_list.append(str(dir_name))
 
     def __len__(self):
-        return self.data.shape[2]
+        return len(self.csv_path_list)
 
     def __getitem__(self, idx):
-        # データ全部を渡す
-        return self.data[:,:,idx], self.label[:,:,idx]
+        raise NotImplementedError
+
+class ThighsTrainValDataset(BaseDataset):
+
+    def __init__(self, path_name):
+        super().__init__(path_name)
+
+        self.label = (
+            'thighs_cant_keep',
+            'thighs_cant_raise',
+            'thighs_cant_slow',
+            'thighs_cant_stop',
+            'thighs_cant_strech',
+            'thighs_correct',
+        )
+
+    def __getitem__(self, idx):
+
+        sig = np.loadtxt(self.csv_path_list[idx], delimiter=',', dtype='float32')
+        sig_norm = sig / 1023
+
+        d, _ = os.path.split(self.csv_path_list[idx])
+        _, classes = os.path.split(d)
+
+        label = self.label.index(classes)
+
+        return sig_norm[np.newaxis,:,:], label
