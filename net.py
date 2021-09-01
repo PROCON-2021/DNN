a77c10e6637180ba6a6fef3d1116463c92be5aa5
ioComk <rw110407@gmail.com>
2021-09-01 17:51:19 +0900

    運動ごとに分類数が違うので出力次元を可変に

--- net.py
+++ net.py
@@ -1,16 +1,15 @@
 import torch.nn as nn
-import torch.nn.functional as F
+import torch as t
 
 class MyModel(nn.Module):
-    def __init__(self):
-        super(MyModel, self).__init__()
+    def __init__(self, out_dim):
+        super().__init__()
 
-        input_size  = 2688
+        input_size  = 2604
         hidden1     = 1024*4
         hidden2     = 1024*1
         hidden3     = 512
         hidden4     = 64
-        output_size = 2
 
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=4, kernel_size=[3, 3], stride=[2, 1], padding=[0, 1]),
@@ -32,26 +31,14 @@ class MyModel(nn.Module):
             nn.Linear(hidden2, hidden2),     nn.BatchNorm1d(hidden2),     nn.LeakyReLU(0.2, inplace=True), nn.Dropout(p=0.3),
             nn.Linear(hidden2, hidden3),     nn.BatchNorm1d(hidden3),     nn.LeakyReLU(0.2, inplace=True), nn.Dropout(p=0.2),
             nn.Linear(hidden3, hidden4),     nn.BatchNorm1d(hidden4),     nn.LeakyReLU(0.2, inplace=True), nn.Dropout(p=0.2),
-            nn.Linear(hidden4, output_size), nn.BatchNorm1d(output_size), nn.LeakyReLU(0.2, inplace=True)
+            nn.Linear(hidden4, out_dim), nn.BatchNorm1d(out_dim), nn.LeakyReLU(0.2, inplace=True)
         )
+
 
     def forward(self, x):
 
-        # conv.
-        output = self.conv(x)
+        x = self.conv(x)  # conv.
+        x = t.flatten(x, 1)  # flatten
+        x = self.dense(x)  # dense
 
-        # Flatten
-        output = output.view(-1, self.num_flat_features(output))
-
-        # dense
-        output = self.dense(output)
-        output = output.view(-1,2)
-        output = F.softmax(output, dim = -1)
-        return output                                                     
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+        return x
