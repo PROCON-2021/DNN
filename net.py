0764ad5622316cd96d4e826b3252d4e6173a5cbf
ioComk <rw110407@gmail.com>
2021-10-02 17:37:18 +0900

    Conv2dモデルの改良

--- net.py
+++ net.py
@@ -72,26 +72,42 @@ class Conv2dModel(ConvModel):
     def __init__(self, out_dim, p=0):
         super().__init__()
 
-        input_size  = 384
-        hidden1     = 128
-        hidden2     = 128
-        hidden3     = 64
+        input_size  = 2240
+        hidden1     = 512
+        hidden2     = 512
+
+        out_channels = [16, 32, 64, 64]
 
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=8, kernel_size=[3, 3], stride=[1, 1], padding=[0, 0]),
-            nn.BatchNorm2d(8),
+            nn.Conv2d(in_channels=3, out_channels=out_channels[0], kernel_size=[3, 3], stride=[1, 1], padding=[1, 1]),
+            nn.BatchNorm2d(out_channels[0]),
             nn.Mish(),
-            nn.MaxPool2d(kernel_size=2, padding=0),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Dropout2d(p=0.5),
 
-            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=[3, 3], stride=[1, 1], padding=[0, 0]),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(in_channels=out_channels[0], out_channels=out_channels[1], kernel_size=[3, 3], stride=[1, 1], padding=[1, 1]),
+            nn.BatchNorm2d(out_channels[1]),
             nn.Mish(),
-            nn.MaxPool2d(kernel_size=2, padding=0),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            nn.Dropout2d(p=0.5),
 
-            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=[3, 3], stride=[1, 1], padding=[0, 0]),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(in_channels=out_channels[1], out_channels=out_channels[2], kernel_size=[3, 3], stride=[1, 1], padding=[0, 0]),
+            nn.BatchNorm2d(out_channels[2]),
             nn.Mish(),
             nn.MaxPool2d(kernel_size=2, padding=0),
+            nn.Dropout2d(p=0.5),
+
+            nn.Conv2d(in_channels=out_channels[2], out_channels=out_channels[3], kernel_size=[2, 2], stride=[1, 1], padding=[0, 0]),
+            nn.BatchNorm2d(out_channels[3]),
+            nn.Mish(),
+            nn.MaxPool2d(kernel_size=2, padding=0),
+            nn.Dropout2d(p=0.5),
+
+            nn.Conv2d(in_channels=out_channels[2], out_channels=out_channels[3], kernel_size=[2, 2], stride=[1, 1], padding=[0, 0]),
+            nn.BatchNorm2d(out_channels[3]),
+            nn.Mish(),
+            nn.MaxPool2d(kernel_size=2, padding=0),
+            nn.Dropout2d(p=0.5),
         )
 
         self.dense = nn.Sequential(
@@ -110,10 +126,5 @@ class Conv2dModel(ConvModel):
             nn.Mish(),
             nn.Dropout(p),
 
-            nn.Linear(hidden2, hidden3),
-            nn.BatchNorm1d(hidden3),
-            nn.Mish(),
-            nn.Dropout(p),
-
-            nn.Linear(hidden3, out_dim),
+            nn.Linear(hidden2, out_dim),
         )
